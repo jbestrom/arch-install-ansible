7643d315c59aa27e2add5e2ac0b35b48094be306
Josh Bestrom <jbestrom@gmail.com>
2022-01-03 19:36:18 -0700

    Test

--- install.sh
+++ install.sh
@@ -6,6 +6,5 @@ pacman -Sy git ansible python-passlib --noconfirm;
 
 git clone https://github.com/jbestrom/arch-install-ansible;
 
-pushd /root/arch-install-ansible
+cd /root/arch-install-ansible
 ansible-playbook -i localhost install.yml
-popd

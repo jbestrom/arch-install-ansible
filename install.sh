7ee5092f0c9207e5ffaea89badbcf705173406e6
Josh Bestrom <jbestrom@gmail.com>
2022-01-03 19:30:28 -0700

    Test

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ pacman -Sy git ansible --noconfirm;
 
 git clone https://github.com/jbestrom/arch-install-ansible;
 
-pushd arch-install-ansible
+pushd /root/arch-install-ansible
+echo $PWD
+echo $CWD
 ansible-playbook -i localhost install.yml
 popd

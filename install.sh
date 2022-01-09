a501cd8f591f066ae888c96234dc945f17be8e12
Josh Bestrom <jbestrom@gmail.com>
2022-01-09 16:41:15 -0700

    Test

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 mount -o remount,size=2G /run/archiso/cowspace;
 
@@ -6,6 +7,4 @@ pacman -Sy git ansible python-passlib --noconfirm;
 
 git clone https://github.com/jbestrom/arch-install-ansible;
 
-pushd arch-install-ansible
-ansible-playbook -i localhost install.yml
-popd
+ansible-playbook -i localhost /root/install.yml
